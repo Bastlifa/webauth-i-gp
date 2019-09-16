16654e9915b7c2670bb36b87a2f8eff98146bcae
Eli Sacks <bastlifa@gmail.com>
2019-09-16 13:29:43 -0400

    register, login

--- index.js
+++ index.js
@@ -16,10 +16,32 @@ server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
-server.post('/api/register', (req, res) => {
-  let user = req.body;
+// server.post('/api/register', (req, res) => {
+//   let user = req.body;
+//   let hashPass
+  
+//   bcryptjs.genSalt(14, function(err, salt)
+//   {
+//     bcryptjs.hash(user.password, salt, function(err, hash)
+//     {
+//       hashPass = hash
+//       Users.add({username: user.username, password: hashPass})
+//         .then(saved => {
+//           res.status(201).json(saved);
+//         })
+//         .catch(error => {
+//           res.status(500).json(error);
+//         });
+//     })
+//   })
+// });
+// })
 
-  Users.add(user)
+server.post('/api/register', (req, res) => {
+  let {username, password} = req.body;
+  const hash = bcryptjs.hashSync(password, 12)
+
+  Users.add({username, password: hash})
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -34,7 +56,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcryptjs.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -53,13 +75,13 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.send(err));
 });
 
-server.get('/hash', (req, res) =>
-{
-  const name = req.query.name
-  // hash the name
-  let hash = bcryptjs.hashSync(name, 12)
-  res.send(`the hash for the ${name} is ${hash}`)
-})
+// server.get('/hash', (req, res) =>
+// {
+//   const name = req.query.name
+//   // hash the name
+//   let hash = bcryptjs.hashSync(name, 14)
+//   res.send(`the hash for the ${name} is ${hash}`)
+// })
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
